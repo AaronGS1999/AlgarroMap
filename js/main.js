8308cb64907073de3575c2866cf14aeb81e8b358
AaronGS1999 <aarongalvez1999@gmail.com>
2025-04-22 10:01:36 +0200

    Cambios en el código .js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Función para cargar los datos .json
 function cargarJSON(url, callback) {
     fetch(url)
         .then(response => response.json())
@@ -7,37 +8,43 @@ function cargarJSON(url, callback) {
 
 const mymap = L.map('mapid').setView([36.93, -1.99], 13);
 
+// Teselas del mapa
 L.tileLayer('https://tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         '<a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; ' +
         '<a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
     errorTileUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAAAQCAYAAAB49x6fAAAAHklEQVR42mNgGAWjgP///zDw//8/AyUY0QYAjKkMBV8Y+Z4AAAAASUVORK5CYII=',
-    keepBuffer: 5
+    keepBuffer: 5,
+    updateWhenIdle: true,
+    reuseTiles: true,     
+    unloadInvisibleTiles: true, 
+    detectRetina: true 
 }).addTo(mymap);
 
-// Definir los íconos según el tipo de árbol
+// Asignación de íconos según el tipo de árbol
 function getIconUrl(punto) {
     const sexo = punto.Sexo ? punto.Sexo.toLowerCase() : '';
 
     if (punto.injertada && punto.injertada.toLowerCase() === 'si') {
         return 'Iconos/injerto.png';
-    } else if (sexo.includes('hermafrodita')) {
-        return 'Iconos/hermafrodita.png';
     } else if (sexo.includes('hembra')) {
         return 'Iconos/hembra.png';
     } else if (sexo.includes('macho')) {
         return 'Iconos/macho.png';
+    } else if (sexo.includes('hermafrodita')) {
+        return 'Iconos/hermafrodita.png';
     } else {
         return 'Iconos/Algarrobo_gris.png';
     }
 }
 
+// Tamaño de los iconos
 function getIconSize() {
     if (window.innerWidth < 768) {
         return [80, 80];
     } else {
-        return [100, 100];
+        return [80, 80];
     }
 }
 
@@ -84,18 +91,18 @@ cargarJSON('Datos/datos.json', function(puntos) {
         totalArboles++;
 
         if (punto.injertada && punto.injertada.toLowerCase() === 'si') {
-            tiposArboles.injertada++;
-        } else if (punto.Sexo && punto.Sexo.toLowerCase().includes('hermafrodita')) {
-            tiposArboles.hermafrodita++;
+            tiposArboles.injertada++; 
         } else if (punto.Sexo && punto.Sexo.toLowerCase().includes('hembra')) {
             tiposArboles.hembra++;
         } else if (punto.Sexo && punto.Sexo.toLowerCase().includes('macho')) {
             tiposArboles.macho++;
+        }  else if (punto.Sexo && punto.Sexo.toLowerCase().includes('hermafrodita')) {
+                tiposArboles.hermafrodita++;
         } else {
             tiposArboles.otros++;
         }
 
-        // Mostrar directamente la imagen ampliada al hacer clic, sin popup
+        // Mostrar ficha al pulsar punto GPS
         const img = new Image();
         img.src = `Fichas/${punto.imagen}`;
         img.onload = function () {
@@ -103,7 +110,7 @@ cargarJSON('Datos/datos.json', function(puntos) {
         };
     });
 
-    // Contenido de la leyenda
+    // Ventana de la leyenda
     const legendContent = `
         <div id="legend-popup-content" style="padding: 20px; background-color: white; border: 1px solid #ccc; border-radius: 5px; font-size: 16px; max-width: 450px; max-height: 650px; overflow-y: auto; text-align: center;">
             <h4 style="margin-top: 0;">Leyenda</h4>
@@ -140,7 +147,7 @@ cargarJSON('Datos/datos.json', function(puntos) {
         </div>
     `;
 
-    // Crear un div para la leyenda y añadirlo al body
+    // div leyenda
     const legendDiv = document.createElement('div');
     legendDiv.id = 'legend-container';
     legendDiv.innerHTML = legendContent;
@@ -155,13 +162,13 @@ cargarJSON('Datos/datos.json', function(puntos) {
     legendDiv.style.border = '1px solid #ccc';
     legendDiv.style.borderRadius = '5px';
     legendDiv.style.padding = '20px';
-    legendDiv.style.zIndex = '1000'; // Asegura que esté por encima del mapa
+    legendDiv.style.zIndex = '1000'; 
     legendDiv.style.maxWidth = '90vw';
     legendDiv.style.maxHeight = '80vh';
     legendDiv.style.overflowY = 'auto';
     legendDiv.style.textAlign = 'center';
 
-    // Añadir evento para cerrar la leyenda
+    // Cerrar leyenda
     const closeButton = document.getElementById('close-legend');
     closeButton.addEventListener('click', function() {
         const legendContainer = document.getElementById('legend-container');
@@ -171,18 +178,17 @@ cargarJSON('Datos/datos.json', function(puntos) {
     });
 });
 
+// Cierre popups
 function ampliarImagen(src) {
     const overlay = document.getElementById('img-overlay');
     const overlayImg = document.getElementById('img-overlay-content');
     overlayImg.src = src;
     overlay.style.display = 'flex';
 
-    // Cierra cualquier popup abierto al ampliar la imagen
     mymap.closePopup();
 }
 
-// Cerrar imagen ampliada y también cualquier popup abierto
 document.getElementById('img-overlay').addEventListener('click', function () {
     this.style.display = 'none';
-    mymap.closePopup(); // Cierra posibles popups residuales
+    mymap.closePopup();
 });
